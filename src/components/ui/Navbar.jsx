42c4db438cab7176fa6718c8feb1a4437b60dc1e
Aakash <vermaaakash1000@gmail.com>
2023-07-22 22:04:27 +0530

    Navabar Updated with links

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 function Navbar() {
     return (
         <nav className='w-full shadow-lg flex flex-row justify-center'>
             <ul className='flex sm:flex-row flex-col '>
-                <li href="#" className='mx-2 p-4 hover:scale-110  transition-transform'>Home</li>
-                <li href="#" className='mx-2 p-4  hover:scale-110 transition-transform'>Transform</li>
-                <li href="#" className='mx-2 p-4  hover:scale-110 transition-transform'>Focal Point</li>
-                <li href="#" className='mx-2 p-4  hover:scale-110 transition-transform'>Effects</li>
-                <li href="#" className='mx-2 p-4  hover:scale-110  transition-transform'>Image Tag</li>
-                <li href="#" className='mx-2 p-4  hover:scale-110 transition-transform'>Optimizations</li>
+                <li className='mx-2 p-4 hover:scale-110 transition-transform'><Link to='/' >Home</Link></li>
+                <li className='mx-2 p-4 hover:scale-110 transition-transform'><Link to='/transform' >Transform</Link></li>
+                <li className='mx-2 p-4 hover:scale-110 transition-transform'>Focal Point</li>
+                <li className='mx-2 p-4 hover:scale-110 transition-transform'>Effects</li>
+                <li className='mx-2 p-4 hover:scale-110 transition-transform'><Link to='/Tagging' >Tagging</Link></li>
+                <li className='mx-2 p-4 hover:scale-110 transition-transform'>Optimizations</li>
             </ul>
         </nav>
     )
