0bb520911986096ef53fdf8c60ea45c219e29dd3
Aakash <vermaaakash1000@gmail.com>
2023-07-22 11:35:55 +0530

    Home Page Created

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import Navbar from '../../components/ui/Navbar';
+import DragandDrop from '../../components/DragandDrop';
+import LightButton from '../../components/LightButton';
+function Home({ childImage, image }) {
+
+
+    return (
+        <div className='flex flex-col h-full'>
+            <DragandDrop onChildImageUpload={childImage} />
+            {
+                image ? <>
+                    <h2 className='text-2xl text-center mt-10'>What do you wanted to do with this image</h2>
+                    <div className='flex justify-center m-10 gap-10'>
+                        <LightButton text="Transform" />
+                        <LightButton text="Focal Point" />
+                        <LightButton text="Effects" />
+                        <LightButton text="Image Tag" />
+                        <LightButton text="Optimization" />
+
+                    </div>
+                </> :
+                    <div className='flex flex-row'>
+                        <div className='w-1/2 flex justify-center'><h2 className='text-center self-center text-container'>Upload Your Image and let the magic happen</h2></div>
+                        <div className="svg-container"></div>
+                    </div>
+            }
+        </div>
+    )
+}
+
+export default Home;
