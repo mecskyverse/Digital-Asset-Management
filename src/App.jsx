73ae9e8a163fea78dab2b713982e2260bcdbacd8
Aakash <vermaaakash1000@gmail.com>
2023-07-22 22:01:41 +0530

    tagging page updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,29 +11,32 @@ import Layout from './components/ui/Layout'
 import NotFound from './components/ui/NotFound'
 import ImageTransformation from './pages/Transform/ImageTransformation'
 import './index.css'
+import Tag from './pages/Imagetagging/Tag'
 
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Layout />}>
-    <Route index element={<Home />} />
-    <Route path='transform' element={<ImageTransformation />} />
 
-    <Route path="*" element={<NotFound />} />
-  </Route>
-)
-)
 function App() {
   const [image, setImage] = useState(null)
   const onImageUpload = (childImage) => {
     setImage(childImage);
-    console.log(childImage);
+    console.log('App', childImage);
   }
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home childImage={onImageUpload} image={image} />} />
+      <Route path='transform' element={<ImageTransformation image={image} />} />
+      <Route path='tagging' element={<Tag image={image} />} />
 
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+  )
   return (
 
     <RouterProvider router={router} />
 
   )
 }
+
 
 export default App
