574d4d92cc77a5e7d2a068fb0fdc41862598b431
Aakash <vermaaakash1000@gmail.com>
2023-08-19 19:29:44 +0530

    Navbar Layout updated

--- src/components/ui/Layout.jsx
+++ src/components/ui/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
     return (
         <>
             <div className="site-wrapper">
-                {/* < Navbar /> */}
+                < Navbar />
                 <Outlet />
 
             </div>
