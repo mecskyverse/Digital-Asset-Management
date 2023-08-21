400e5dba70ecfeda6555ca336cc79ed254b55c59
Aakash <vermaaakash1000@gmail.com>
2023-08-21 13:06:56 +0530

    Functionalities edited

--- src/pages/Transform/ImageTransformation.jsx
+++ src/pages/Transform/ImageTransformation.jsx
@@ -1,76 +1,31 @@
-import React, { useState, createRef, useEffect } from 'react'
-import { useSelector } from 'react-redux';
-import Cropper from 'react-cropper'
-import LightButton from '../../components/LightButton';
+import React, { useState } from 'react'
+import Crop from '../../components/Crop/Crop'
+import Overlay from '../../components/Overlay/Overlay'
+import Frames from '../../components/Frames/Frames';
 import '../../../node_modules/cropperjs/dist/cropper.css';
-import DragandDrop from '../../components/DragandDrop';
-const defaultSrc =
-    "https://images.contentstack.io/v3/assets/blt7359e2a55efae483/blt549032e6f65d185e/648afff350d8edcbdf2f1baa/hero-composable-3_(1).svg";
-function ImageTransformation({ image }) {
+function ImageTransformation() {
 
-    const [trial, setTrial] = useState(false);
-    const cropperRef = createRef();
-    const imageData = useSelector((state) => state.image.imageData);
-    console.log('Data', imageData);
-    const handleDownload = () => {
-        if (typeof cropperRef.current?.cropper !== "undefined") {
-            const link = document.createElement("a");
-            link.href = cropperRef.current?.cropper.getCroppedCanvas().toDataURL();
-            link.download = "image.png";
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }
-    };
-    if (imageData == null && trial == false) {
-        return (
-            <>
-                <div className='flex flex-col justify-center items-center'>
-                    <div className='text-3xl text-center mt-5'>You have not uploaded Image Please Upload! OR </div>
-                    <LightButton text='Use Default' onClick={() => setTrial(true)} />
-                </div>
-                <DragandDrop />
-            </>
-        )
-    }
-    if (image == null && trial == true) {
-        image = defaultSrc
-    }
+
+    const [editOption, setEditOption] = useState('Crop')
     return (
-        <div className='flex flex-row w-full'>
-            <div className='w-1/2'>
-                <Cropper
-                    ref={cropperRef}
-                    style={{ height: '100vh', width: "100%" }}
-                    zoomTo={0.5}
-                    initialAspectRatio={1}
-                    preview=".img-preview"
-                    src={imageData}
-                    viewMode={1}
-                    minCropBoxHeight={10}
-                    minCropBoxWidth={10}
-                    background={false}
-                    responsive={true}
-                    autoCropArea={1}
-                    checkOrientation={false}
-                    guides={true}
-                />
-            </div>
-            <div className='w-1/2 flex flex-col'>
-                <div className="self-center w-96">
-                    <h1 className='text-3xl text-center mt-5'>Preview</h1>
-                    <div
-                        className="img-preview w-full h-96"
-                    />
-                </div>
-                <div className=" self-center mt-5 h-96" >
+        <div className='flex flex-row w-full bg-edit h-[93vh]'>
+            {editOption === 'Crop' ? <Crop /> : editOption === 'Overlay' ? <Overlay /> : <Frames />}
+            <div className='w-1/6 flex flex-col border border-red-200 absolute right-0 h-[93vh]'>
+                <div className='text-3xl text-center mb-10 mt-4 font-bold'>Edit Options</div>
+                <hr />
 
-                    <LightButton text='Download' onClick={handleDownload} />
-                    {/* <img style={{ width: "100%" }} src={cropData} alt="cropped" /> */}
-                    {/* <button onClick={() => handleDownload(cropData)}>Download</button> */}
-                </div>
+
+                {['Crop', 'Overlay', 'Frames'].map((item) => {
+                    return (
+                        <>
+                            {console.log(editOption === item)}
+                            <button onClick={() => setEditOption(item)} className={`${editOption == item ? 'bg-white' : 'hover:bg-slate-300'} h-28 text-xl`}>{item}</button >
+                            <hr />
+                        </>
+                    )
+                })}
             </div>
-        </div>
+        </div >
     )
 }
 
