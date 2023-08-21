68fa1098800c613706ac00315230569e73e61703
Aakash <vermaaakash1000@gmail.com>
2023-08-21 13:02:06 +0530

    Frames and Overlay option added

---
+++ src/components/Frames/Frames.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Frames() {
+    return (
+        <div>Frames</div>
+    )
+}
+
+export default Frames
---
+++ src/components/Overlay/Overlay.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Overlay() {
+    return (
+        <div>Overlay</div>
+    )
+}
+
+export default Overlay
