4831ab8e381405b733332cef0fd5572066889011
Aakash <vermaaakash1000@gmail.com>
2023-08-25 16:53:51 +0530

    server updated with CORS policy

--- httpServer/Server.js
+++ httpServer/Server.js
@@ -5,6 +5,17 @@ const port = process.env.PORT || 8081;
 
 app.use(express.json({limit: '50mb'}));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+app.get('/status', (req, res) => {
+    res.send('Everything is fine!');
+});
+
+
 app.post('/image', (req, res) => {
     const image = req.body.image;
     const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
