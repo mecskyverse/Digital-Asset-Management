1c1e61b45b51dc9e2bcb49c76244f42354d018a4
Aakash <vermaaakash1000@gmail.com>
2023-08-25 18:49:13 +0530

    Server Cors policy updated

--- httpServer/Server.js
+++ httpServer/Server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 8081;
 
 app.use(express.json({limit: '50mb'}));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors());
 
 app.get('/status', (req, res) => {
     res.send('Everything is fine!');
