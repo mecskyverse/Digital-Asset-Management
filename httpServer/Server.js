70d501545ae3d091e16f34b76a8da77b21ce4d2f
Aakash <vermaaakash1000@gmail.com>
2023-08-25 14:05:21 +0530

    http server created

---
+++ httpServer/Server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const port = process.env.PORT || 8081;
+
+app.use(express.json({limit: '50mb'}));
+
+app.post('/image', (req, res) => {
+    const image = req.body.image;
+    const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
+    const filename = `image-${Date.now()}.png`;
+    fs.writeFile(filename, base64Data, 'base64', (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error saving image');
+        } else {
+            res.send(`http://localhost:${port}/${filename}`);
+        }
+    });
+});
+
+app.use(express.static('.'));
+
+app.listen(port, () => {
+    console.log(`Server listening at ${port}`);
+});
