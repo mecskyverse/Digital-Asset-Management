fc9f8b404f490c0ba470d425046af6b01ef43e25
Aakash <vermaaakash1000@gmail.com>
2023-08-27 13:21:48 +0530

    Redux added in effects page

--- src/pages/Effects/Effects.jsx
+++ src/pages/Effects/Effects.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateImageData } from '../../redux/features/imageSlice';
+import LightButton from '../../components/LightButton';
 import Slider from '../../components/Slider'
 import SidebarItem from '../../components/EffectsSidebar'
-
+import { CssTwoTone } from '@mui/icons-material';
 const DEFAULT_OPTIONS = [
     {
         name: 'Brightness',
@@ -79,6 +82,19 @@ function App() {
     const [selectedOptionIndex, setSelectedOptionIndex] = useState(0)
     const [options, setOptions] = useState(DEFAULT_OPTIONS)
     const selectedOption = options[selectedOptionIndex]
+    const [imageDimension, setImageDimension] = useState({ height: 0, width: 0 })
+    const [loading, setLoading] = useState(false)
+
+    const imageData = useSelector((state) => state.image.imageData);
+    const canvasRef = useRef();
+    const imageRef = useRef();
+    const dispatch = useDispatch();
+
+    const handleImageLoad = () => {
+        const currHeight = imageRef.current.naturalHeight;
+        const currWidth = imageRef.current.naturalWidth;
+        setImageDimension({ width: currWidth, height: currHeight })
+    }
 
     function handleSliderChange({ target }) {
         setOptions(prevOptions => {
@@ -89,20 +105,58 @@ function App() {
         })
     }
 
-    function getImageStyle() {
+    function getImageStyle(forCanvas = false) {
         const filters = options.map(option => {
-            console.log('option', option)
             return `${option.property}(${option.value}${option.unit})`
         })
-        console.log({ filter: filters.join('') })
+        if (forCanvas) {
+            return filters.join('')
+        }
         return { filter: filters.join(' ') }
+    }
+
+    const handleDownload = (finalize = false) => {
+        setLoading(true);
+        const canvasElement = canvasRef.current;
+        const imageElement = imageRef.current;
+        if (imageElement && canvasElement) {
+            const ctx = canvasElement.getContext('2d')
+
+
+
+            canvasElement.width = imageDimension.width
+            canvasElement.height = imageDimension.height
+
+            ctx.filter = getImageStyle(true);
+            ctx.drawImage(imageElement, 0, 0);
+            if (finalize == false) {
+                // Create a download link
+                const link = document.createElement('a');
+                link.download = 'image_with_effects.png';
+                link.href = canvasElement.toDataURL('image/png');
+                // Trigger the download
+                link.click();
+            }
+            else {
+                const base64ImageData = canvasElement.toDataURL();
+                dispatch(updateImageData(base64ImageData))
+            }
+
+
+
+        }
+        setLoading(false)
     }
 
 
     return (
         <div className="flex flex-row h-[93vh] bg-gradient-to-r from-blue-400 via-purple-200 to-pink-500">
-            <section className='flex flex-col items-center h-full w-4/5'>
-                <img src='https://source.unsplash.com/EwKXn5CapA4' className="main-image" style={getImageStyle()} />
+            <section className='flex flex-col items-center h-full w-4/5 bg-black bg-opacity-50'>
+                <img src={imageData} className="main-image " style={getImageStyle()} onLoad={handleImageLoad} ref={imageRef} />
+                <canvas
+                    ref={canvasRef}
+                    className='hidden '
+                />
                 <Slider
                     min={selectedOption.range.min}
                     max={selectedOption.range.max}
@@ -121,6 +175,18 @@ function App() {
                         />
                     )
                 })}
+                <button
+                    className="w-40 self-center mt-10 border bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => handleDownload(true)}
+                >
+                    {loading ? 'loading...' : 'Finalize Frame'}
+                </button>
+                <span className='self-center mt-10'>
+                    <LightButton
+                        text={loading ? 'loading...' : 'Download'}
+                        onClick={() => handleDownload(false)}
+                    />
+                </span>
             </div>
 
 
