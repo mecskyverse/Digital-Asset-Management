b64b36be0372df105fcd6bb3553e89e66f94b634
Aakash <vermaaakash1000@gmail.com>
2023-08-27 19:28:37 +0530

    reductant code removed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  Link
 } from "react-router-dom"
 import Home from './pages/Home/Home'
 import Layout from './components/UI/Layout'
@@ -14,17 +13,10 @@ import Effects from './pages/Effects/Effects'
 import './index.css'
 import Tag from './pages/Imagetagging/Tag'
 
-
-
 function App() {
-  const [image, setImage] = useState(null)
-  const onImageUpload = (childImage) => {
-    setImage(childImage);
-    console.log('App', childImage);
-  }
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-      <Route index element={<Home childImage={onImageUpload} />} />
+      <Route index element={<Home />} />
       <Route path='edit' element={<ImageTransformation />} />
       <Route path='imagetagging' element={<Tag />} />
       <Route path='effects' element={<Effects />} />
