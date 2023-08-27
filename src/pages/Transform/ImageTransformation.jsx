c3e351256547a3652e767e0d3af4525a25bf0389
Aakash <vermaaakash1000@gmail.com>
2023-08-27 13:22:05 +0530

    Undo redo option added

--- src/pages/Transform/ImageTransformation.jsx
+++ src/pages/Transform/ImageTransformation.jsx
@@ -1,10 +1,30 @@
 import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { undoImageData, redoImageData } from '../../redux/features/imageSlice';
 import Crop from '../../components/Crop/Crop'
 import Overlay from '../../components/Overlay/Overlay'
 import Frames from '../../components/Frames/Frames';
 import '../../../node_modules/cropperjs/dist/cropper.css';
+import { FaUndo, FaRedo } from 'react-icons/fa'
 function ImageTransformation() {
     const [editOption, setEditOption] = useState('Crop')
+    const dispatch = useDispatch();
+    const imageDataIndex = useSelector((state) => state.image.imageDataIndex);
+    const imageDataHistory = useSelector((state) => state.image.imageDataHistory)
+    const totalStates = imageDataHistory.length
+
+    const handleUndo = () => {
+        if (imageDataIndex > 0) {
+            dispatch(undoImageData());
+        }
+    };
+
+    const handleRedo = () => {
+        if (imageDataIndex < totalStates - 1) {
+            dispatch(redoImageData());
+        }
+    };
+
     return (
         <div className='flex flex-row w-full bg-edit h-[93vh]'>
             {editOption === 'Crop' ? <Crop /> : editOption === 'Overlay' ? <Overlay /> : <Frames />}
@@ -21,6 +41,14 @@ function ImageTransformation() {
                         </>
                     )
                 })}
+                <div className='flex gap-10 justify-center mt-10 text-gray-800 text-xl'>
+                    <button onClick={handleUndo} className='hover:bg-slate-600 bg-opacity-25 p-5' disabled={imageDataIndex === 0}>
+                        <FaUndo />
+                    </button>
+                    <button onClick={handleRedo} className='hover:bg-slate-600 bg-opacity-25 p-5' disabled={imageDataIndex === totalStates - 1}>
+                        <FaRedo />
+                    </button>
+                </div>
             </div>
         </div >
     )
