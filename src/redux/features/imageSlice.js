024096346ae1d84ffaeb8d2b0d1b4d4b5af57ac0
Aakash <vermaaakash1000@gmail.com>
2023-08-27 13:25:37 +0530

    Undo and Redo functionality added

--- src/redux/features/imageSlice.js
+++ src/redux/features/imageSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+    imageDataIndex: -1,
     imageName: null,
-    imageData : null
+    imageData : null,
+    imageDataHistory: [],
 
 }
 const imageSlice = createSlice({
@@ -10,19 +12,44 @@ const imageSlice = createSlice({
     initialState,
     reducers:{
         setImageData : (state, action) => {
+             // Clearing the redo history when setting new data
+            state.imageDataHistory = state.imageDataHistory.slice(0, state.imageDataIndex + 1);
+
             state.imageName = action.payload.imageName;
             state.imageData = action.payload.imageData;
+
+              // Adding the current state to the history and setting the initial ImageData index as 0
+            state.imageDataHistory.push({ imageData: state.imageData });
+            state.imageDataIndex = 0;
         } ,
         updateImageData: (state, action) => {
             state.imageData = action.payload;
+            state.imageDataHistory.push({imageData: state.imageData })
+            state.imageDataIndex++;
           },
-        clearImageData: (state, action) => {
+        clearImageData: (state) => {
             state.imageName = null
             state.imageData = null
-        }
-    }
+            state.imageDataIndex = -1;
+            state.imageDataHistory = [];
+        },
+        undoImageData: (state) => {
+            if(state.imageDataIndex > 0){
+                state.imageDataIndex--;
+                const {imageData} = state.imageDataHistory[state.imageDataIndex];
+                state.imageData = imageData;
+            }
+        },
+        redoImageData:(state) => {
+            if(state.imageDataIndex< state.imageDataHistory.length -1 ){
+                state.imageDataIndex++;
+                const {imageData} =  state.imageDataHistory[state.imageDataIndex];
+                state.imageData = imageData
+            }
+        },
+    },
 })
 
-export const {setImageData, updateImageData, clearImageData} = imageSlice.actions;
+export const {setImageData, updateImageData, clearImageData, undoImageData, redoImageData} = imageSlice.actions;
 export default imageSlice.reducer;
 
